1921ed1677ccda8d550d49c656f99825bfb775e7
Delaksan-Sritharan <delaksan@gmail.com>
2024-08-08 02:43:55 +0530

    Add files via upload

---
+++ Password generator.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_password(length):
+  characters = string.ascii_letters + string.digits + string.punctuation+string.digits
+  password = ''.join(random.choice(characters) for _ in range(length))
+  return password
+
+if __name__ == "__main__":
+  length = int(input("Enter password length: "))
+  password = generate_password(length)
+  print("Password:", password)
